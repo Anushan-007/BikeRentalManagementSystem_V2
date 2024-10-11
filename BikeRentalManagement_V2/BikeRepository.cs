03012afaeb4bd878ee021ed64a722a57092375ab
Maheswaran A. <UT01317@unicomtic.com>
2024-10-11 12:58:50 +0530

    14 ques

---
+++ BikeRentalManagement_V2/BikeRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace BikeRentalManagement_V2
+{
+    internal class BikeRepository
+    {
+        static string ConnectionString = "Server=(localdb)\\MSSQLLocalDB; Initial Catalog=BikeRentalManagement";
+
+        
+        public void CreateBike(Bike bike)
+            {
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                string query = "INSERT INTO Bikes(BikeId, Brand, Model, RentalPrice) VALUES(@BikeId, @Brand, @Model, @RentalPrice);";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    Console.WriteLine("Enter the ID");
+                    cmd.Parameters.AddWithValue("@BikeId", bike.bikeId);
+                    Console.WriteLine("Enter the Brand");
+                    cmd.Parameters.AddWithValue("@Brand", bike.Brand);
+                    Console.WriteLine("Enter the Model");
+                    cmd.Parameters.AddWithValue("@Model", bike.Model);
+                    Console.WriteLine("Enter the RentalPrice");
+                    cmd.Parameters.AddWithValue("@RentalPrice", bike.RentalPrice);
+                    cmd.ExecuteNonQuery();
+
+                    Console.WriteLine("Successfully Created");
+
+                }
+
+            }
+        }
+
+        //public void ReadBikes()
+        //{
+
+        //    using (SqlConnection conn = new SqlConnection(ConnectionString))
+        //    {
+        //        conn.Open();
+        //        string query = "SELECT * FROM Bikes";
+        //        using (SqlCommand cmd = new SqlCommand(query, conn))
+        //        {
+        //            SqlDataReader reader = new SqlDataReader()
+        //                while (reader.Read())
+        //            {
+        //                cmd.Parameters.AddWithValue("@BikeId", bike.bikeId);
+        //            }
+                    
+                    
+
+
+
+        //        }
+
+        //    }
+        //}
+
+    }
+}
--- BikeRentalManagement_V2/Program.cs
+++ BikeRentalManagement_V2/Program.cs
@@ -10,7 +10,45 @@ namespace BikeRentalManagement_V2
     {
         static void Main(string[] args)
         {
+            var Repo = new BikeRepository();
 
+            var status = true;
+            while (status)
+            {
+                Console.WriteLine("................................");
+                Console.WriteLine("Bike Rental Management System: ");
+                Console.WriteLine("1. Add a Bike: ");
+                Console.WriteLine("2. View All Bikes: ");
+                Console.WriteLine("3. Update a Bike: ");
+                Console.WriteLine("4. Delete a Bike: ");
+                Console.WriteLine("5. Exit: ");
+                Console.Write("Choose an option: ");
+
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        Repo.CreateBike();
+                        break;
+                    case "2":
+                        Repo.ReadBikes();
+                        break;
+                    case "3":
+                        Repo.UpdateBike();
+                        break;
+                    case "4":
+                        Repo.DeleteBike();
+                        break;
+                    case "5":
+                        status = false;
+                        break;
+                    default:
+                        Console.Clear();
+                        Menu();
+                        break;
+                }
+
+            }
         }
     }
 }
